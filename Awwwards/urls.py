85e9a08d27575d00a7b1556d385af230ef8e3457
imekenye <ismekenye@gmail.com>
2019-05-25 06:04:31 -0700

    create profile model

--- Awwwards/urls.py
+++ Awwwards/urls.py
@@ -22,11 +22,13 @@ from django.conf.urls.static import static
 from accounts import views as user_views
 
 urlpatterns = [
-                  path('admin/', admin.site.urls),
-                  path('', views.home, name='projects-home'),
-                  path('projects/', include('projects.urls')),
-                  path('register/', user_views.register, name='register'),
-                  path('profile/', user_views.profile, name='profile'),
-                  path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
-                  path('logout/', auth_views.LogoutView.as_view(template_name='accounts/logout.html'), name='logout'),
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('admin/', admin.site.urls),
+    path('', views.home, name='projects-home'),
+    path('projects/', include('projects.urls')),
+    path('register/', user_views.register, name='register'),
+    path('profile/', user_views.profile, name='profile'),
+    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='accounts/logout.html'), name='logout'),
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

df28c9a74a712fde49b99ad314287bf853e53891
imekenye <ismekenye@gmail.com>
2019-05-25 18:31:34 -0700

    initial deployment

--- Awwwards/settings.py
+++ Awwwards/settings.py
@@ -51,7 +51,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'mysecretkey'
 # SECRET_KEY = 'xp$)4&qat_g1)%txik6n+&&wh@rja@r&u#+ll3^=umgf@o@*f$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -110,16 +109,16 @@ WSGI_APPLICATION = 'Awwwards.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'projectsdb',
-        'USER': 'studiopx',
-        'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'projectsdb',
+#         'USER': 'studiopx',
+#         'PASSWORD': '',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
